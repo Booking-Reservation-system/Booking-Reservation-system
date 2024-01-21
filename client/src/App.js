2c919d0aa4820d1e8548878c15f49aa958f0fe9a
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-01-21 10:55:05 +0700

    add navbar component to app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+import NavBar from "./components/navbar/NavBar";
+
+// const BrowserRouter = createBrowserRouter({
+//
+// });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <NavBar />
+            <div className="text-rose-500 text-2xl">
+            </div>
+        </>
+    );
 }
 
 export default App;
