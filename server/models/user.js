0c3c8956b744135127896bbadf6e4b693b017ba5
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-01-29 15:06:32 +0700

    add user model

---
+++ server/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const {models} = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+    },
+    email: { // unique
+        type: String,
+    },
+    emailVerified: {
+        type: Date,
+    },
+    image: {
+        type: String,
+    },
+    hashedPassword: {
+        type: String,
+    },
+    favoriteIds: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+    }],
+    accounts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Account',
+            required: true,
+    }],
+    listings: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Listing',
+        required: true,
+    }],
+    reservations: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Reservation',
+        required: true,
+    }]
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
