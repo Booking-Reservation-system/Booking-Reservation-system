53b4c6d4fbf5400070b04646f6dd003a40eefdc0
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-02-05 22:47:08 +0700

    update middleware auth

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/controllers/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
