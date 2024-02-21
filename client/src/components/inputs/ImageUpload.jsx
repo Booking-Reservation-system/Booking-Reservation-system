c17f2ef7499a7a98ce2a86f37328c2ed00376a4c
Bui Nguyen Tung Lam <106130570+TLam2908@users.noreply.github.com>
2024-02-21 22:34:06 +0700

    Add step 4-5

--- client/src/components/inputs/ImageUpload.jsx
+++ client/src/components/inputs/ImageUpload.jsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef } from "react";
-import {TbPhotoPlus} from "react-icons/tb";
+import { useCallback, useEffect, useRef } from "react";
+import { TbPhotoPlus } from "react-icons/tb";
 
 const ImageUpload = (props) => {
-
   const { value, onChange } = props;
 
   const cloudinaryRef = useRef();
@@ -22,21 +21,27 @@ const ImageUpload = (props) => {
     );
   }, []);
 
- 
+
   return (
     <>
       <div
         onClick={() => widgetRef.current.open()}
         className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-col justify-center items-center gap-4 text-neutral-600"
       >
-        <TbPhotoPlus size={50}/>
-        <div className="font-semibold text-lg">
-            Click to Upload
-        </div>
+        <TbPhotoPlus size={50} />
+        <div className="font-semibold text-lg">Click to Upload</div>
         {value && (
-            <div className="absolute inset-0 w-full h-full">
-                <img alt="Upload" fill style={{objectFit: 'cover'}} src={value}></img>
-            </div>
+          <div
+            className="absolute inset-0 w-full h-full"
+            onClick={handleUpload}
+          >
+            <img
+              alt="Upload"
+              fill
+              style={{ objectFit: "cover" }}
+              src={value}
+            ></img>
+          </div>
         )}
       </div>
     </>
--- client/src/components/modals/RentModal.jsx
+++ client/src/components/modals/RentModal.jsx
@@ -9,6 +9,10 @@ import ImageUpload from "../inputs/ImageUpload";
 import CategoryInput from "../inputs/CategoryInput";
 import CountrySelect from "../inputs/CountrySelect";
 import Counter from "../inputs/Counter";
+import Input from "../inputs/Input"
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const STEPS = {
   CATEGORY: 0,
@@ -21,8 +25,9 @@ const STEPS = {
 
 const RentModal = () => {
   const rentModal = useRentModal();
-
+  const navigate = useNavigate();
   const [step, setStep] = useState(STEPS.CATEGORY);
+  const [isLoading, setIsLoading] = useState(false);
 
   const {
     register,
@@ -74,6 +79,27 @@ const RentModal = () => {
   const onNext = (value) => {
     setStep((value) => value + 1);
   };
+
+  const onSubmit = (data) => {
+    if (step !== STEPS.PRICE) {
+      return onNext();
+    }
+    setIsLoading(true);
+    axios.post('', data)
+      .then(() => {
+        toast.success('Your place has been added');
+        navigate('/') // redirect to the home page
+        reset()
+        setStep(STEPS.CATEGORY)
+        rentModal.onClose()
+      })
+      .catch(() => {
+        toast.error('Something went wrong')
+      })
+      .finally(() => {
+        setIsLoading(false)
+      })
+  }
 
   const actionLabel = useMemo(() => {
     if (step === STEPS.PRICE) {
@@ -171,13 +197,63 @@ const RentModal = () => {
         <ImageUpload value={imageSrc} onChange={() => setCustomValue('imageSrc', imageSrc)}/>
       </div>
     )
+    console.log(imageSrc);
+  }
+
+  if (step === STEPS.DESCRIPTION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="How would you describe your place?"
+          subtitle="Short and sweet works best"
+        />
+        <Input
+          id="title"
+          label="Title"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+        <hr/>
+        <Input
+          id="description"
+          label="Description"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
+  }
+
+  if (step === STEPS.PRICE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="Now, set your price"
+          subtitle="How much do you charge per night?"
+        />
+        <Input
+          id="price"
+          label="Price"
+          formatPrice
+          type="number"
+          disabled={isLoading}
+          register={register}
+          errors={errors}
+          required
+        />
+      </div>
+    )
   }
 
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={onNext}
+      onSubmit={onSubmit}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
