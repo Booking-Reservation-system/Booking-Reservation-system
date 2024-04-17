5c6d779e620f2e1cc000bdf8d1e1c7b0c0e67ad9
Bui Nguyen Tung Lam <106130570+TLam2908@users.noreply.github.com>
2024-04-17 10:44:47 +0700

    add booked date and fav page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,14 @@ import IndexPage from "./pages/IndexPage";
 import ErrorPage from "./pages/ErrorPage";
 import ListingPage from "./pages/ListingPage";
 import TripPage from "./pages/TripPage";
-
+import FavouritePage from "./pages/FavouritePage";
 function App() {
   return (
     <Routes>
       <Route index element={<IndexPage />} errorElement={<ErrorPage />} />
       <Route path="listing/:placeId" element={<ListingPage/>} />
       <Route path="trips" element={<TripPage/>} />
+      <Route path="favourites" element={<FavouritePage/>} />
     </Routes>
   );
 }
---
+++ client/src/pages/FavouritePage.jsx
@@ -0,0 +1,15 @@
+
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+
+const FavouritePage = () => {
+    return (
+        <Container>
+            <div className="pt-[120px]">
+                <Heading title="Favourites" subtitle="Your favourite places"/>
+            </div>
+        </Container>
+    )
+}
+
+export default FavouritePage;
