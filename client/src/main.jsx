51d0872ac2b78cb959b14c6d3b66fae66cc75db7
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-05-07 10:01:03 +0700

    Merge branch 'main' of https://github.com/Booking-Reservation-system/Booking-Reservation-system

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar.jsx";
-import ToasterProvider from "./providers/ToasterProvider.jsx";
-import RentModal from "./components/modals/RentModal.jsx";
-import LoginModal from "./components/modals/LoginModal.jsx";
-import RegisterModal from "./components/modals/RegisterModal.jsx";
-import EditPlaceModal from "./components/modals/EditPlaceModal.jsx";
+import RootLayout from "./pages/RootLayout.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
       <BrowserRouter>
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        <EditPlaceModal/>
-        <Navbar />
-        <App />
+        <RootLayout />
       </BrowserRouter>
   </React.StrictMode>
 );
