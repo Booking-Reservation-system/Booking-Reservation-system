12315333f451327cdcc7034dd96a3d0123e54476
MinhNGD76 <minhngd67@gmail.com>
2024-05-12 23:44:28 +0700

    profile (p1)

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,4 +1,5 @@
 import getAllPlaces from "../action/getAllPlaces";
+import {GoCheck} from "react-icons/go"
 
 const ProfilePage = () => {
   return (
@@ -22,7 +23,7 @@ const ProfilePage = () => {
               ></img>
               <div>
                 <h1 className="font-bold text-xl">Bui Nguyen Tung Lam</h1>
-                <p className="text-md">Host / Organization</p>
+                <p className="text-md font-bold">Host / Organization</p>
               </div>
             </div>
             <div className="text-center">
@@ -31,28 +32,30 @@ const ProfilePage = () => {
             </div>
           </div>
 
-          <div className="flex flex-row w-[380px] h-[280px] justify-around items-center border-2 border-gray-400 rounded-[20px] gap-6 px-6 py-6">
-          <div className="flex flex-col justify-center items-center text-center gap-4">
-            <img
-              className="rounded-full"
-              width={100}
-              height={100}
-              src="/placeholder.jpg"
-            ></img>
-            <div>
-              <h1 className="font-bold text-xl">Bui Nguyen Tung Lam</h1>
-              <p className="text-md">Host / Organization</p>
-            </div>
-          </div>
-          <div className="text-center">
-            <h1 className="font-bold text-4xl text-[#db0b63]">1</h1>
-            <p>years experience in Booking App</p>
+          <div className="flex flex-row w-[380px] h-[280px] justify-around items-start border-2 border-gray-400 rounded-[20px] gap-6 px-6 py-6">
+          <div>
+            <p className="font-bold text-xl" style={{ marginBottom: '16px' }}>Bui Nguyen Tung Lam's confirmed information</p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Identity</span>
+            </p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Email address</span>
+            </p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Phone number</span>
+            </p>
           </div>
         </div>
 
         </div>
 
-        <div>Information</div>
+        <div>
+          <h1 className="font-bold text-xxl" style={{fontSize: '32px'}}>About Bui Nguyen Tung Lam</h1>
+          
+        </div>
       </div>
     </div>
   );
