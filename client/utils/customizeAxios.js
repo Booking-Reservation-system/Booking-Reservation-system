00161767b2989fba8b06526257bccf7434dbca09
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-05-12 10:26:58 +0700

    change const to let

--- client/utils/customizeAxios.js
+++ client/utils/customizeAxios.js
@@ -1,61 +1,61 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
-  timeout: 10000,
-  headers: {
-    "Content-Type": "application/json",
-  },
-  //withCredentials: true
+    baseURL: "http://localhost:8080",
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+    //withCredentials: true
 });
 
 export default instance;
 
 instance.interceptors.request.use(
-  async function (config) {
-    // Do something before request is sent
-    const accessToken = localStorage.getItem("accessToken");
-    if (accessToken) {
-      const expires_in = localStorage.getItem("expiresAt");
-      if (expires_in * 1000 < new Date.getTime()) {
-        const refreshToken = localStorage.getItem("refreshToken");
-        const provider = localStorage.getItem("provider");
-        if (provider) {
-          accessToken = await axios.post("http://localhost:8080/auth/google/refresh", {refreshToken}, {withCredentials: true})
-            .then((response) => {
-              const {accessToken, expires_in} = response.data;
-              console.log('Google refresh');
-              localStorage.setItem("accessToken", accessToken);
-              localStorage.setItem("expiresAt", expires_in);
-              return accessToken;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        } else {
-          accessToken = await axios.post("http://localhost:8080/auth/refresh", {refreshToken})
-            .then((response) => {
-              const {accessToken, expires_in} = response.data;
-              console.log('jwt refresh')
-              localStorage.setItem("accessToken", accessToken);
-              localStorage.setItem("expiresAt", expires_in);
-              return accessToken;
-            })
-            .catch((error) => {
-              console.log(error);
-            });
+    async function (config) {
+        // Do something before request is sent
+        let accessToken = localStorage.getItem("accessToken");
+        if (accessToken) {
+            const expires_in = localStorage.getItem("expiresAt");
+            if (expires_in * 1000 < new Date.getTime()) {
+                const refreshToken = localStorage.getItem("refreshToken");
+                const provider = localStorage.getItem("provider");
+                if (provider) {
+                    accessToken = await axios.post("http://localhost:8080/auth/google/refresh", {refreshToken}, {withCredentials: true})
+                        .then((response) => {
+                            const {accessToken, expires_in} = response.data;
+                            console.log('Google refresh');
+                            localStorage.setItem("accessToken", accessToken);
+                            localStorage.setItem("expiresAt", expires_in);
+                            return accessToken;
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                } else {
+                    accessToken = await axios.post("http://localhost:8080/auth/refresh", {refreshToken})
+                        .then((response) => {
+                            const {accessToken, expires_in} = response.data;
+                            console.log('jwt refresh')
+                            localStorage.setItem("accessToken", accessToken);
+                            localStorage.setItem("expiresAt", expires_in);
+                            return accessToken;
+                        })
+                        .catch((error) => {
+                            console.log(error);
+                        });
+                }
+
+            }
+            config.headers.Authorization = 'Bearer ' + accessToken
+
         }
-
-      }
-    config.headers.Authorization = 'Bearer ' + accessToken 
-
+        return config;
+    },
+    function (error) {
+        // Do something with request error
+        return Promise.reject(error);
     }
-    return config;
-  },
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
 );
 
 // instance.axios.response.use(
