d3271bc58477809f093fe4babf69e2d3181487a2
MinhNGD76 <minhngd67@gmail.com>
2024-05-14 02:23:12 +0700

    UI fixed in ProfilePage and add EditProfilePage

---
+++ client/src/pages/EditProfilePage.jsx
@@ -0,0 +1,110 @@
+import getAllPlaces from "../action/getAllPlaces";
+
+const EditProfilePage = () => {
+    return (
+    //     <div
+    //   className="pt-[150px] gap-4 max-w-[1540px] 
+    //   mx-auto 
+    //   xl:px-20
+    //   md:px-10
+    //   sm:px-2
+    //   px-4"
+    // >
+
+    // </div>
+    <div>
+        <h2 className="font-bold text-xxl text-start" style={{fontSize: '32px', marginBottom: '50px'}}>Personal info</h2>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Full name</p>
+              <p style={{marginBottom: '20px'}}>Nguyen Dinh Minh</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Email address</p>
+              <p style={{marginBottom: '20px'}}>admin@gmail.com</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Phone number</p>
+              <p style={{marginBottom: '20px'}}>0943242243</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+      
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Where I live</p>
+              <p style={{marginBottom: '20px'}}>Hanoi, Vietname</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Where I went to college</p>
+              <p style={{marginBottom: '20px'}}>Posts and Telecommunications Institute of Technology</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>My work</p>
+              <p style={{marginBottom: '20px'}}>大学生</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Language I speak</p>
+              <p style={{marginBottom: '20px'}}>English and Vietnamese</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>Mu fun fact</p>
+              <p style={{marginBottom: '20px'}}>I'm not funny</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+              <p style={{marginBottom: '0px'}}>My description</p>
+              <p style={{marginBottom: '20px'}}>Something ...</p>
+            </div>
+            <button style={{ marginLeft: '50px' }}>Edit</button>
+        </div>
+
+        <hr/>
+      </div>
+    );
+};
+
+export default EditProfilePage;
---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,114 @@
+import getAllPlaces from "../action/getAllPlaces";
+import {GoCheck} from "react-icons/go"
+import { LuGraduationCap } from "react-icons/lu";
+import { PiGlobeStand } from "react-icons/pi";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { LuLanguages } from "react-icons/lu";
+import { FaRegLightbulb } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoPhonePortraitOutline } from "react-icons/io5";
+
+const ProfilePage = () => {
+  return (
+    <div
+      className="pt-[150px] gap-4 max-w-[1540px] 
+      mx-auto 
+      xl:px-20
+      md:px-10
+      sm:px-2
+      px-4"
+    >
+      <div className="flex flex-row gap-20">
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-row w-[380px] h-[280px] justify-around items-center shadow-2xl rounded-[20px] gap-6 px-6 py-6">
+            <div className="flex flex-col justify-center items-center text-center gap-4">
+              <img
+                className="rounded-full"
+                width={100}
+                height={100}
+                src="/placeholder.jpg"
+              ></img>
+              <div>
+                <h1 className="font-bold text-xl">Bui Nguyen Tung Lam</h1>
+                <p className="text-md font-bold">Host / Organization</p>
+              </div>
+            </div>
+            <div className="text-center">
+              <h1 className="font-bold text-4xl text-[#db0b63]">1</h1>
+              <p>years experience in Booking App</p>
+            </div>
+          </div>
+
+          <div className="flex flex-row w-[380px] h-[280px] justify-around items-start border-2 border-gray-400 rounded-[20px] gap-6 px-6 py-6">
+          <div> {/* Refactor to tailwind!!*/}
+            <p className="font-bold text-xl" style={{ marginBottom: '16px' }}>Bui Nguyen Tung Lam's confirmed information</p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Identity</span>
+            </p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Email address: ...</span> {/* Display email address*/}
+            </p>
+            <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <GoCheck className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Phone number: ...</span> {/* Dispay phone number*/}
+            </p>
+          </div>
+        </div>
+
+        </div>
+
+        <div>
+          <h1 className="font-bold text-xxl" style={{fontSize: '32px', marginBottom: '30px'}}>About Bui Nguyen Tung Lam</h1>
+
+          <div> {/* change to table like display later*/}
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <MdOutlineEmail  className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>My email: something@gmail.com</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <IoPhonePortraitOutline  className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>My phone number: 0943422784</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <PiGlobeStand className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Where I live: Hanoi, Vietnam</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <LuGraduationCap className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Where I went to college: Posts and Telecommunications Institute of Technology</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <MdOutlineWorkOutline className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>My work: 大学生</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '10px'}}>
+              <LuLanguages className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>Language I speak: English, Vietnamese</span>
+          </p>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '50px'}}>
+              <FaRegLightbulb className="ml-2" size={30}/>
+              <span style={{ marginLeft: '25px' }}>My fun fact: 大きなおっぱいJKが大好き</span>
+          </p>
+          </div>
+
+          <div>
+          <p className="text-xl text-center" style={{ display: 'flex', alignItems: 'left', justifyContent: 'left', marginBottom: '50px'}}>
+            Description about yourself ...
+          </p>
+          </div>
+
+          <hr style={{marginBottom: '50px'}}/>
+
+          <div>
+          <p className="font-bold text-xl" style={{ marginBottom: '16px' }}>Bui Nguyen Tung Lam's listings</p>
+          {/* add logic to here later*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
