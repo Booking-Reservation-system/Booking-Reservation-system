02676bda01a9a9bc30521f48ba46c9dcca8a6a6a
Bui Nguyen Tung Lam <106130570+TLam2908@users.noreply.github.com>
2024-05-14 15:58:11 +0700

    Hide show password

--- client/src/components/modals/ChangePasswordModal.jsx
+++ client/src/components/modals/ChangePasswordModal.jsx
@@ -1,48 +1,53 @@
 import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import Modal from "../modals/Modal";
-import Heading from "../Heading"
-import Input from "../inputs/Input"
+import Heading from "../Heading";
+import Input from "../inputs/Input";
 import useEditProfileModal from "../../hooks/useEditProfileModal";
 import useAuth from "../../hooks/useAuth";
 import axios from "axios";
 import toast from "react-hot-toast";
+
 const ChangePasswordModal = () => {
   const editProfile = useEditProfileModal();
   const { authToken } = useAuth();
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
     register,
     handleSubmit,
     setValue,
     watch,
-    formState: {errors},
+    formState: { errors },
     reset,
-} = useForm({
+  } = useForm({
     defaultValues: {
-        oldPassword: "",
-        newPassword: "",
+      oldPassword: "",
+      newPassword: "",
     },
-});
+  });
 
-const oldPassword = watch("oldPassword");
-const newPassword = watch("newPassword");
+  const oldPassword = watch("oldPassword");
+  const newPassword = watch("newPassword");
 
-const setCustomValue = (id, value) => {
+  const setCustomValue = (id, value) => {
     setValue(id, value, {
-        shouldValidate: true,
-        shouldDirty: true,
-        shouldTouch: true,
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
     });
-}
+  };
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading title="Change your password" subtitle="Input carefully!"/>
+      <Heading title="Change your password" subtitle="Input carefully!" />
       <Input
         id="oldPassword"
         label="Old password"
         value={oldPassword}
+        showPassword={showPassword}
+        setShowPassword={() => setShowPassword(!showPassword)}
+        type={showPassword ? "text" : "password"}
         onChange={(value) => setCustomValue("oldPassword", value)}
         errors={errors}
         required
@@ -51,8 +56,11 @@ const setCustomValue = (id, value) => {
       <Input
         id="newPassword"
         label="New password"
+        showPassword={showPassword}
+        setShowPassword={() => setShowPassword(!showPassword)}
+        type={showPassword ? "text" : "password"}
         value={newPassword}
-        onChange={(value) => setCustomValue("newPassword", value)} 
+        onChange={(value) => setCustomValue("newPassword", value)}
         errors={errors}
         required
         register={register}
@@ -63,31 +71,31 @@ const setCustomValue = (id, value) => {
   const onSubmit = async (data) => {
     console.log(data);
     try {
-        const response = await axios.put(
-            "http://localhost:8080/api/profile/change-password",
-            data,
-            {
-                headers: {
-                    Authorization: "Bearer " + authToken,
-                }
-            }
-        )
-        toast.success("Profile updated successfully");
-        editProfile.onChangePasswordClose();
+      const response = await axios.put(
+        "http://localhost:8080/api/profile/change-password",
+        data,
+        {
+          headers: {
+            Authorization: "Bearer " + authToken,
+          },
+        }
+      );
+      toast.success("Profile updated successfully");
+      editProfile.onChangePasswordClose();
     } catch (error) {
-        console.log(error);
-        toast.error("Fail to update password");
+      console.log(error);
+      toast.error("Fail to update password");
     }
-  }
+  };
 
   return (
-    <Modal 
-        isOpen={editProfile.isChangePassword} 
-        onClose={editProfile.onChangePasswordClose} 
-        title="Change password"
-        actionLabel="Save Changes"
-        body={bodyContent}
-        onSubmit={handleSubmit(onSubmit)}
+    <Modal
+      isOpen={editProfile.isChangePassword}
+      onClose={editProfile.onChangePasswordClose}
+      title="Change password"
+      actionLabel="Save Changes"
+      body={bodyContent}
+      onSubmit={handleSubmit(onSubmit)}
     />
   );
 };
